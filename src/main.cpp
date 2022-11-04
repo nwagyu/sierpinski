c4d4627a83c8731c7d9b457db2fb011407e929ec
Julie Mason <julie.t.mason@outlook.com>
2022-11-04 18:19:46 +0000

    Update nwlink command

--- src/main.cpp
+++ src/main.cpp
@@ -3,23 +3,21 @@
 extern const char eadk_app_name[] __attribute__((section(".rodata.eadk_app_name"))) = "Sierpinski";
 extern const uint32_t eadk_api_level __attribute__((section(".rodata.eadk_api_level"))) = 0;
 
-constexpr int DisplayWidth = 320;
-constexpr int DisplayHeight = 240;
 constexpr int ScrollBufferHeight = 10;
 
 int main(int argc, char * argv[]) {
-  int numberRows[2][DisplayWidth] = {0};
-  EADK::Display::Color pixelRow[DisplayWidth];
+  int numberRows[2][EADK_SCREEN_WIDTH] = {0};
+  EADK::Display::Color pixelRow[EADK_SCREEN_WIDTH];
 
   int * previousRow = numberRows[0];
   int * currentRow = numberRows[1];
 
-  EADK::Display::Color scrollPixelBuffer[DisplayWidth*ScrollBufferHeight];
+  EADK::Display::Color scrollPixelBuffer[EADK_SCREEN_WIDTH*ScrollBufferHeight];
 
   for (int j=0; true; j++) {
     previousRow = numberRows[j%2];
     currentRow = numberRows[(j+1)%2];
-    for (int i=0; i<DisplayWidth; i++) {
+    for (int i=0; i<EADK_SCREEN_WIDTH; i++) {
       if (i==0) {
         currentRow[i] = 1;
       } else {
@@ -27,14 +25,14 @@ int main(int argc, char * argv[]) {
       }
       pixelRow[i] = (currentRow[i]%2) ? EADK::Display::Color(0xFFFF) : EADK::Display::Color(0x0);
     }
-    if (j<DisplayHeight) {
-      EADK::Display::pushRect(EADK::Display::Rect(0, (j%DisplayHeight), DisplayWidth, 1), pixelRow);
+    if (j<EADK_SCREEN_HEIGHT) {
+      EADK::Display::pushRect(EADK::Display::Rect(0, (j%EADK_SCREEN_HEIGHT), EADK_SCREEN_WIDTH, 1), pixelRow);
     } else {
-      for (int i=0; i<DisplayHeight/ScrollBufferHeight; i++) {
-        EADK::Display::pullRect(EADK::Display::Rect(0, i*ScrollBufferHeight+1, DisplayWidth, ScrollBufferHeight), scrollPixelBuffer);
-        EADK::Display::pushRect(EADK::Display::Rect(0, i*ScrollBufferHeight, DisplayWidth, ScrollBufferHeight), scrollPixelBuffer);
+      for (int i=0; i<EADK_SCREEN_HEIGHT/ScrollBufferHeight; i++) {
+        EADK::Display::pullRect(EADK::Display::Rect(0, i*ScrollBufferHeight+1, EADK_SCREEN_WIDTH, ScrollBufferHeight), scrollPixelBuffer);
+        EADK::Display::pushRect(EADK::Display::Rect(0, i*ScrollBufferHeight, EADK_SCREEN_WIDTH, ScrollBufferHeight), scrollPixelBuffer);
       }
-      EADK::Display::pushRect(EADK::Display::Rect(0, DisplayHeight-1, DisplayWidth, 1), pixelRow);
+      EADK::Display::pushRect(EADK::Display::Rect(0, EADK_SCREEN_HEIGHT-1, EADK_SCREEN_WIDTH, 1), pixelRow);
     }
   }
 }
